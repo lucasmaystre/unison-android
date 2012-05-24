67aca6efb1afb0412f7cab13924fe8ec4e4eef6f
Lucas Maystre <lucas@maystre.ch>
2012-05-24 15:20:42 +0200

    Improvements in the preferences screen.
    The user can now change his username and lookup his user ID.

--- src/ch/epfl/unison/ui/PrefsActivity.java
+++ src/ch/epfl/unison/ui/PrefsActivity.java
@@ -5,11 +5,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.util.Log;
+import ch.epfl.unison.AppData;
 import ch.epfl.unison.R;
+import ch.epfl.unison.api.JsonStruct;
+import ch.epfl.unison.api.UnisonAPI;
+import ch.epfl.unison.api.UnisonAPI.Error;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 public class PrefsActivity extends SherlockPreferenceActivity {
+
+    private static final String TAG = "ch.epfl.unison.PrefsActivity";
 
     private BroadcastReceiver logoutReceiver = new BroadcastReceiver() {
         @Override
@@ -28,6 +36,12 @@ public class PrefsActivity extends SherlockPreferenceActivity {
 
         this.setTitle(R.string.activity_title_prefs);
         this.addPreferencesFromResource(R.xml.prefs);
+
+        this.findPreference("nickname").setOnPreferenceChangeListener(
+                new NicknameChangeListener());
+
+        this.findPreference("uid").setSummary(
+                String.format("your UID is: %d", AppData.getInstance(this).getUid()));
     }
 
     @Override
@@ -35,5 +49,27 @@ public class PrefsActivity extends SherlockPreferenceActivity {
         super.onDestroy();
         this.unregisterReceiver(this.logoutReceiver);
     }
+
+    private class NicknameChangeListener implements Preference.OnPreferenceChangeListener {
+
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            final String newNick = newValue.toString();
+            AppData data = AppData.getInstance(PrefsActivity.this);
+            data.getAPI().setNickname(data.getUid(), newNick,
+                    new UnisonAPI.Handler<JsonStruct.Success>() {
+
+                public void callback(JsonStruct.Success struct) {
+                    Log.i(TAG, String.format("changed nickname to %s", newNick));
+                }
+
+                public void onError(Error error) {
+                    Log.w(TAG, String.format("couldn't set new nickname %s", newNick));
+                }
+            });
+            return true;
+        }
+
+    }
+
 
 }
