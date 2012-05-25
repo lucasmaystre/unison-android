574346ffc1800994da1c1b030ffc5c031f3dcd6f
Lucas Maystre <lucas@maystre.ch>
2012-05-25 18:04:22 +0200

    Small cosmetic changes.
    Various UI improvements:
    - forbid landscape orientation when needed
    - better default texts (no fake artist & titles anymore...)
    - errors when signing up are shown as toasts
    - Corrected RoomsActivity title

--- src/ch/epfl/unison/ui/SignupActivity.java
+++ src/ch/epfl/unison/ui/SignupActivity.java
@@ -4,12 +4,12 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 import ch.epfl.unison.AppData;
 import ch.epfl.unison.R;
 import ch.epfl.unison.api.JsonStruct;
@@ -22,7 +22,6 @@ public class SignupActivity extends SherlockActivity {
 
     private static final String TAG = "ch.epfl.unison.SignupActivity";
 
-    private TextView errors;
     private Button submitBtn;
     private TextView email;
     private TextView password;
@@ -34,8 +33,6 @@ public class SignupActivity extends SherlockActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.signup);
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        this.errors = (TextView) this.findViewById(R.id.errors);
 
         this.submitBtn = (Button) this.findViewById(R.id.submitBtn);
         this.submitBtn.setOnClickListener(new SubmitListener());
@@ -79,24 +76,28 @@ public class SignupActivity extends SherlockActivity {
         });
     }
 
+    public void showError(String error) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+    }
+
     public void handleError(UnisonAPI.Error error) {
         if (error.hasJsonError()) {
             if (UnisonAPI.ErrorCodes.MISSING_FIELD == error.jsonError.error) {
-                this.errors.setText("Fields are missing.");
+                this.showError("Fields are missing.");
 
             } else if (UnisonAPI.ErrorCodes.EXISTING_USER == error.jsonError.error) {
-                this.errors.setText("User already exists (e-mail address in use).");
+                this.showError("User already exists (e-mail address in use).");
 
             } else if (UnisonAPI.ErrorCodes.INVALID_EMAIL == error.jsonError.error) {
-                this.errors.setText("E-mail address is invalid.");
+                this.showError("E-mail address is invalid.");
 
             } else if (UnisonAPI.ErrorCodes.INVALID_PASSWORD == error.jsonError.error) {
-                this.errors.setText("Password is invalid.");
+                this.showError("Password is invalid.");
             }
             return;
         }
         // Last resort.
-        this.errors.setText("Unable to create new user, please try again.");
+        this.showError("Unable to create new user, please try again.");
     }
 
     private class SubmitListener implements OnClickListener {
@@ -105,17 +106,13 @@ public class SignupActivity extends SherlockActivity {
             if (TextUtils.isEmpty(email.getText())
                     || TextUtils.isEmpty(password.getText())
                     || TextUtils.isEmpty(password2.getText())) {
-                errors.setText("Please fill out all the fields.");
-                errors.setVisibility(View.VISIBLE);
+                showError("Please fill out all the fields.");
             } else if (!password.getText().toString().equals(password2.getText().toString())) {
-                Log.d(TAG, password.getText() + " " + password2.getText());
-                errors.setText("Passwords don't match.");
-                errors.setVisibility(View.VISIBLE);
+                showError("Passwords don't match.");
             } else if (password.length() < 6) {
-                errors.setText("Password is too short.");
-                errors.setVisibility(View.VISIBLE);
+                showError("Password is too short.");
             } else if (!tou.isChecked()) {
-                errors.setText("You need to agree to the terms of use.");
+                showError("You need to agree to the terms of use.");
             } else {
                 signup(email.getText().toString(), password.getText().toString());
             }
